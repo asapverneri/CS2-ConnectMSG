d15b1c60858bb328c5810d7000bfde5468e1b626
asapverneri <mattikaikkola@gmail.com>
2024-09-13 04:10:06 +0300

    v1.3
    - Added welcome message
    - Added country to lang

--- cs2connectmsg.cs
+++ cs2connectmsg.cs
@@ -2,20 +2,39 @@ using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Entities;
+using MaxMind.GeoIP2;
+using MaxMind.GeoIP2.Exceptions;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace CS2connectmsg;
 
 [MinimumApiVersion(247)]
-public class CS2connectmsg : BasePlugin
+public class CS2connectmsgConfig : BasePluginConfig
 {
-    public override string ModuleName => "CS2-ConnectMSG";
+    [JsonPropertyName("PlayerWelcomeMessage")] public bool PlayerWelcomeMessage { get; set; } = true;
+    [JsonPropertyName("Timer")] public float Timer { get; set; } = 5.0f;
+
+}
+public class CS2connectmsg : BasePlugin, IPluginConfig<CS2connectmsgConfig>
+{
+    public override string ModuleName => "ConnectMSG";
     public override string ModuleDescription => "Simple connect/disconnect messages";
     public override string ModuleAuthor => "verneri";
-    public override string ModuleVersion => "1.0.2";
+    public override string ModuleVersion => "1.3";
 
+    public static Dictionary<ulong, bool> LoopConnections = new Dictionary<ulong, bool>();
+
+    public CS2connectmsgConfig Config { get; set; } = new();
+
+    public void OnConfigParsed(CS2connectmsgConfig config)
+    {
+        Config = config;
+    }
     public override void Load(bool hotReload)
     {
-        Console.WriteLine($"[{ModuleName}] loaded successfully! (Version {ModuleVersion})");
+        Console.WriteLine($"loaded successfully! (Version {ModuleVersion})");
     }
 
     [GameEventHandler]
@@ -23,19 +42,34 @@ public class CS2connectmsg : BasePlugin
     {
         if (@event == null) return HookResult.Continue;
         var player = @event.Userid;
+        if (player == null || !player.IsValid || player.IsBot) return HookResult.Continue;
+        var steamid = player.SteamID;
         var Name = player.PlayerName;
+        string country = GetCountry(player.IpAddress?.Split(":")[0] ?? "Unknown");
 
-        if (player.IsValid)
+        if (LoopConnections.ContainsKey(steamid))
         {
-            Console.WriteLine($"[{ModuleName}] {Name} has connected!");
-            Server.PrintToChatAll($"{Localizer["playerconnect", Name]}");
-
+            LoopConnections.Remove(steamid);
         }
+
+        Console.WriteLine($"[{ModuleName}] {Name} has connected!");
+        Server.PrintToChatAll($"{Localizer["playerconnect", Name, country]}");
+
+        if (Config.PlayerWelcomeMessage)
+        {
+            AddTimer(Config.Timer, () =>
+            {
+                player.PrintToChat($"{Localizer["playerwelcomemsg", Name]}");
+                player.PrintToChat($"{Localizer["playerwelcomemsgnextline"]}");
+            });
+        }
+        
 
         return HookResult.Continue;
     }
+
     [GameEventHandler]
-    private HookResult OnPlayerDisconnectPre(EventPlayerDisconnect @event, GameEventInfo info)
+    public HookResult OnPlayerDisconnectPre(EventPlayerDisconnect @event, GameEventInfo info)
     {
         if (@event == null) return HookResult.Continue;
         info.DontBroadcast = true;
@@ -48,25 +82,51 @@ public class CS2connectmsg : BasePlugin
     {
         if (@event == null) return HookResult.Continue;
         var player = @event.Userid;
+        if (player == null || !player.IsValid || player.IsBot) return HookResult.Continue;
         var reason = @event.Reason;
         var Name = player.PlayerName;
+        string country = GetCountry(player.IpAddress?.Split(":")[0] ?? "Unknown");
 
         info.DontBroadcast = true;
 
-        if (player.IsValid)
-        {
+        
             if(reason == 54 || reason == 55 || reason == 57)
             {
-                info.DontBroadcast = true;
-                return HookResult.Continue;
+                if (!LoopConnections.ContainsKey(player.SteamID))
+                {
+                    LoopConnections.Add(player.SteamID, true);
+                }
+                if (LoopConnections.ContainsKey(player.SteamID))
+                {
+                    return HookResult.Continue;
+                }
             }
 
-            Console.WriteLine($"[{ModuleName}] {Name} has disconnected!");
-            Server.PrintToChatAll($"{Localizer["playerdisconnect", Name]}");
+        Console.WriteLine($"[{ModuleName}] {Name} has disconnected!");
+        Server.PrintToChatAll($"{Localizer["playerdisconnect", Name, country]}");
 
-        }
+        
 
         return HookResult.Continue;
+    }
+
+
+    public string GetCountry(string ipAddress)
+    {
+        try
+        {
+            using var reader = new DatabaseReader(Path.Combine(ModuleDirectory, "GeoLite2-Country.mmdb"));
+            var response = reader.Country(ipAddress);
+            return response?.Country?.IsoCode ?? "Unknown";
+        }
+        catch (AddressNotFoundException)
+        {
+            return "Unknown";
+        }
+        catch
+        {
+            return "Unknown";
+        }
     }
 
 }
